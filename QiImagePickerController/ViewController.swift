a96091634b6a7e438c09b0757934ddaef39619e3
qinwanli <Wally_qin@163..com>
2020-02-11 15:10:40 +0800

    Initial Commit

---
+++ QiImagePickerController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QiImagePickerController
+//
+//  Created by apple on 2020/2/11.
+//  Copyright © 2020 QiShare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
